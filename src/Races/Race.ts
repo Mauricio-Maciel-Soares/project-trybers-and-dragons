e832798efac155db09d619d7dc4506e686476d60
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2022-06-27 15:16:32 -0300

    add race class

---
+++ src/Races/Race.ts
@@ -0,0 +1,11 @@
+class Race {
+  name: string;
+  dexterity: number;
+
+  constructor(name: string, dexterity: number) {
+    this.name = name;
+    this.dexterity = dexterity;
+  }
+}
+
+export default Race;
