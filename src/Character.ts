4ab19e56ff5f8e5be10128837901ae47b34d800b
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2022-06-28 20:56:31 -0300

    add character class

---
+++ src/Character.ts
@@ -0,0 +1,48 @@
+import Archetype, { Mage } from './Archetypes';
+import Energy from './Energy';
+import Fighter from './Fighter';
+import Race, { Elf } from './Races';
+import getRandomInt from './utils';
+
+class Character implements Fighter {
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
+
+  constructor(name: string) {
+    this._archetype = new Mage(name);
+    this._strength = getRandomInt(1, 10);
+    this._defense = getRandomInt(1, 10);
+    this._dexterity = getRandomInt(1, 10);
+    this._energy = {
+      amount: getRandomInt(1, 10),
+      type_: this._archetype.energyType,
+    };
+    this._race = new Elf(name, this._dexterity);
+    this._lifePoints = this._race.maxLifePoints;
+    this._maxLifePoints = this._race.maxLifePoints / 2;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = this._defense - attackPoints;
+    
+    if (damage > 0) {
+      this._lifePoints -= damage;
+    }
+    if (this._lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+    return this._lifePoints;
+  }
+
+  attack(enemy: Fighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+}
+
+export default Character;
